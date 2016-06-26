b847f78e6d4c017c3c494b1f37df5bb15c760ad9
Jonas Tepe <jonasprogrammer@gmail.com>
2016-06-26 16:17:47 +0200

    untabify chapter4

--- chapter4/cp_holes.c
+++ chapter4/cp_holes.c
@@ -13,43 +13,43 @@ int main(int argc, char *argv[])
     ssize_t num_read;
     
     if (argc != 3 || strcmp(argv[1], "--help") == 0)
-	usageErr("%s src dest\n", argv[0]);
+        usageErr("%s src dest\n", argv[0]);
 
     in_fd = open(argv[1], O_RDONLY);
     if (in_fd == -1)
-	errExit("open src file");
+        errExit("open src file");
 
     out_fd = open(argv[2], O_WRONLY | O_CREAT,
-		  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+                  S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (out_fd == -1)
-	errExit("open target file");
+        errExit("open target file");
 
     while ((num_read = read(in_fd, buf, BUFSIZE)) > 0) {
-	off_t i = 0, j;
+        off_t i = 0, j;
 
-	while (i < num_read) {
-	    for (j = i; j < num_read && buf[j] != '\0'; j++);
+        while (i < num_read) {
+            for (j = i; j < num_read && buf[j] != '\0'; j++);
 
-	    size_t non_zero_bytes = (size_t) j - i;
-	    if (write(out_fd, &buf[i], non_zero_bytes) != non_zero_bytes)
-		errExit("write to dest file");
+            size_t non_zero_bytes = (size_t) j - i;
+            if (write(out_fd, &buf[i], non_zero_bytes) != non_zero_bytes)
+                errExit("write to dest file");
 
-	    for (i = j; i < num_read && buf[i] == '\0'; i++);
+            for (i = j; i < num_read && buf[i] == '\0'; i++);
 
-	    off_t hole_length = i - j;
-	    if (hole_length != 0) {
-		if (lseek(out_fd, hole_length, SEEK_CUR) == -1)
-		    errExit("lseek on dest file");
-	    }
-	}
+            off_t hole_length = i - j;
+            if (hole_length != 0) {
+                if (lseek(out_fd, hole_length, SEEK_CUR) == -1)
+                    errExit("lseek on dest file");
+            }
+        }
     }
 
     if (num_read == -1)
-	errExit("read from src file");
+        errExit("read from src file");
 
     if (close(in_fd) == -1)
-	errExit("close src file");
+        errExit("close src file");
     if (close(out_fd) == -1)
-	errExit("close dest file");
+        errExit("close dest file");
 }
 
--- chapter4/tee.c
+++ chapter4/tee.c
@@ -13,33 +13,33 @@ int main(int argc, char *argv[])
     ssize_t bytes_read, bytes_written;
     
     if (argc < 2 || argc > 3)
-	usageErr("%s [-a] file\n", argv[0]);
+        usageErr("%s [-a] file\n", argv[0]);
 
     if ((opt = getopt(argc, argv, ":a")) != -1) {
-	switch (opt) {
-	case 'a':
-	    filename = argv[2];
-	    oflags |= O_APPEND;
-	    break;
-	default: usageErr("%s [-a] file\n", argv[0]);
-	}
+        switch (opt) {
+        case 'a':
+            filename = argv[2];
+            oflags |= O_APPEND;
+            break;
+        default: usageErr("%s [-a] file\n", argv[0]);
+        }
     } else {
-	filename = argv[1];
-	oflags |= O_TRUNC;
+        filename = argv[1];
+        oflags |= O_TRUNC;
     }
     
     if ((fd = open(filename, oflags, mode)) == -1)
-	errExit("open file");
+        errExit("open file");
 
     while ((bytes_read = read(STDIN_FILENO, (void *) buf, BUFSIZE)) != 0) {
-	if (bytes_read == -1)
-	    errExit("read stdin");
-	
-	if ((bytes_written = write(STDOUT_FILENO, (void *) buf, (size_t) bytes_read) != bytes_read))
-	    errExit("write stdout");
+        if (bytes_read == -1)
+            errExit("read stdin");
+        
+        if ((bytes_written = write(STDOUT_FILENO, (void *) buf, (size_t) bytes_read) != bytes_read))
+            errExit("write stdout");
 
-	if ((bytes_written = write(fd, (void *) buf, (size_t) bytes_read)) != bytes_read)
-	    errExit("write file");
+        if ((bytes_written = write(fd, (void *) buf, (size_t) bytes_read)) != bytes_read)
+            errExit("write file");
     }
 
     close(fd);
